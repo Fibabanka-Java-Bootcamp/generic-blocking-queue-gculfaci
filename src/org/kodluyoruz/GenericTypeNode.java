b41fe6b35328f795039b0293b98a5e3a06c44daf
gculfaci <gculfaci@gmail.com>
2021-01-05 21:33:32 +0300

    Queue implementation added without threads

---
+++ src/org/kodluyoruz/GenericTypeNode.java
@@ -0,0 +1,78 @@
+package org.kodluyoruz;
+
+
+public class GenericTypeNode<T> {
+    private T anyValue;
+    private GenericTypeNode<T> nextNode;
+    private GenericTypeNode<T> head;
+    private GenericTypeNode<T> tail;
+    private GenericTypeNode<T> temp;
+
+
+    public GenericTypeNode(T anyValue) {
+        this.anyValue = anyValue;
+    }
+
+    public GenericTypeNode(T anyValue,GenericTypeNode<T> nextNode){
+        this.anyValue = anyValue;
+        this.nextNode = nextNode;
+    }
+
+    public T getValue(){
+        return anyValue;
+    }
+
+    public void setValue(T anyValue) {
+        this.anyValue = anyValue;
+    }
+
+    public GenericTypeNode<T> getNextNode(){
+        return nextNode;
+    }
+
+    public void setNextNode(GenericTypeNode<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public void add(T anyValue){
+
+        GenericTypeNode<T> node = new GenericTypeNode<T>(anyValue);
+        if(head== null){
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.setNextNode(node);
+            tail = node;
+        }
+    }
+    public void showAllQueue(){
+        temp = head;
+        while(temp != null){
+            System.out.print("Queue : - " + temp.getValue()+ " - ");
+            temp = temp.getNextNode();
+
+        }
+        System.out.println("");
+    }
+
+    public T peek(){
+        
+        if(head != null){
+            return head.getValue();
+        }
+        else return null;
+
+    }
+
+    public T poll(){
+        T anyValue = null;
+        if(head != null){
+            GenericTypeNode<T> node = head;
+            head = head.getNextNode();
+            anyValue = node.getValue();
+        }
+        return anyValue;
+    }
+ 
+}
--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -1,8 +1,32 @@
 package org.kodluyoruz;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+      GenericTypeNode<Integer> myQueue = new GenericTypeNode<>(0);
+      myQueue.add(6);
+      myQueue.add(1);
+      myQueue.add(2);
+      myQueue.showAllQueue();
+      System.out.println(myQueue.poll() + " polled in queue");
+      System.out.println(myQueue.peek() + " is first element of queue ");
+      myQueue.showAllQueue();
+
+      System.out.println(" ");
+
+      GenericTypeNode<String> myQueue2 = new GenericTypeNode<>(null);
+      myQueue2.add("Gökhan");
+      myQueue2.add("Ali");
+      myQueue2.add("Tolga");
+      myQueue2.add("Mehmet");
+      myQueue2.showAllQueue();
+      System.out.println(myQueue2.poll() + " polled in queue");
+      System.out.println(myQueue2.peek() + " is first element of queue ");
+      myQueue2.showAllQueue();
+
+    
     }
 }
